a5021024441d223a0e69700830679cfbe12252ad
DenysDenylson <DenysDenylson@gmail.com>
2015-10-13 01:09:03 -0400

    Primera prueba pasando

---
+++ lib/tennis.rb
@@ -0,0 +1,9 @@
+class Tennis
+	def scorer
+		"0 - 0"
+	end
+	
+	def jugador1_anota
+		"15 - 0"
+	end
+end
---
+++ spec/tennis_spec.rb
@@ -0,0 +1,16 @@
+require "tennis"
+
+describe Tennis do
+	it "empieza con score en 0 - 0" do
+		tennis = Tennis.new
+		result = tennis.scorer
+		expect(result).to eq "0 - 0"
+	end
+	
+	# it "jugador1 anota el score esta en 15 - 0" do
+	# 	tennis = Tennis.new
+	# 	tennis.jugador1_anota
+	# 	result = tennis.scorer
+	# 	expect(result).to eq "15 - 0"
+	# end
+end
