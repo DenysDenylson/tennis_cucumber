314170cce2c302eb38fd1424e5d29199a6dfbc95
DenysDenylson <DenysDenylson@gmail.com>
2015-10-13 03:08:51 -0400

    Marcadores individuales de cada jugador finalizadas

--- lib/tennis.rb
+++ lib/tennis.rb
@@ -13,11 +13,23 @@ class Tennis
 			@marcador = "15 - 0"
 		elsif @marcador == "15 - 0"
 			@marcador = "30 - 0"
+		elsif @marcador == "30 - 0"
+			@marcador = "40 - 0"
+		elsif @marcador == "40 - 0"
+			@marcador = "Ganaste jugador1"
 		end
 	end
 	
 	def jugador2_anota
-		@marcador = "0 - 15"
+		if @marcador == "0 - 0"
+			@marcador = "0 - 15"
+		elsif @marcador == "0 - 15"
+			@marcador = "0 - 30"
+		elsif @marcador == "0 - 30"
+			@marcador = "0 - 40"
+		elsif @marcador == "0 - 40"
+			@marcador = "Ganaste jugador2"
+		end
 	end
 	
 	
--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -6,28 +6,65 @@ describe Tennis do
 		@tennis = Tennis.new
 	end
 	
-	it "empieza con score en 0 - 0" do
-		result = @tennis.scorer
-		expect(result).to eq "0 - 0"
+	context "Inicializacion del juego" do
+		it "empieza con score en 0 - 0" do
+			expect(@tennis.scorer).to eq "0 - 0"
+		end
 	end
 	
-	it "jugador1 anota el score esta en 15 - 0" do
-		@tennis.jugador1_anota
-		result = @tennis.scorer
-		expect(result).to eq "15 - 0"
+	context "Anotaciones de Jugador 1 " do
+		it "jugador1 anota el score esta en 15 - 0" do
+			@tennis.jugador1_anota
+			expect(@tennis.scorer).to eq "15 - 0"
+		end
+		
+		it "jugador1 anota 2 veces, el score esta 30 - 0" do
+			@tennis.jugador1_anota
+			@tennis.jugador1_anota
+			expect(@tennis.scorer).to eq "30 - 0"
+		end
+		
+		it "jugador1 anota 3 veces, el score esta 40 - 0" do
+			@tennis.jugador1_anota
+			@tennis.jugador1_anota
+			@tennis.jugador1_anota
+			expect(@tennis.scorer).to eq "40 - 0"
+		end
+		
+		it "jugador1 anota 4 veces seguidas, el score esta 'Ganaste jugador1' " do
+			@tennis.jugador1_anota
+			@tennis.jugador1_anota
+			@tennis.jugador1_anota
+			@tennis.jugador1_anota
+			expect(@tennis.scorer).to eq "Ganaste jugador1"
+		end
 	end
 	
-	it "jugador1 anota 2 veces, el score esta 30 - 0" do
-		@tennis.jugador1_anota
-		@tennis.jugador1_anota
-		result = @tennis.scorer
-		expect(result).to eq "30 - 0"
+	context "Anotaciones de Jugador 2 " do	
+		it "jugador2 anota 1 vez, el score esta 0 - 15" do
+			@tennis.jugador2_anota
+			expect(@tennis.scorer).to eq "0 - 15"
+		end
+		
+		it "jugador2 anota 2 veces, el score esta 0 - 30" do
+			@tennis.jugador2_anota
+			@tennis.jugador2_anota
+			expect(@tennis.scorer).to eq "0 - 30"
+		end
+		
+		it "jugador2 anota 3 veces, el score esta 40 - 0" do
+			@tennis.jugador2_anota
+			@tennis.jugador2_anota
+			@tennis.jugador2_anota
+			expect(@tennis.scorer).to eq "0 - 40"
+		end
+		
+		it "jugador2 anota 4 veces seguidas, el score esta 'Ganaste jugador2' " do
+			@tennis.jugador2_anota
+			@tennis.jugador2_anota
+			@tennis.jugador2_anota
+			@tennis.jugador2_anota
+			expect(@tennis.scorer).to eq "Ganaste jugador2"
+		end
 	end
-	
-	it "jugador2 anota 1 vez, el score esta 0 - 15" do
-		@tennis.jugador2_anota
-		result = @tennis.scorer
-		expect(result).to eq "0 - 15"
-	end
-	
 end
